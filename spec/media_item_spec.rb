d8631162455c6a038cdb1e6f3d9c5daf69fcf29a
Seth Deckard <seth@deckard.us>
2015-01-17 15:17:41 -0600

    replace chars in spec that are invalid in 1.9

--- spec/media_item_spec.rb
+++ spec/media_item_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe M3u8::MediaItem do
   it 'should provide m3u8 format representation' do
     hash = { type: 'AUDIO', group: 'audio-lo', language: 'fre',
-             name: 'Français', auto: true, default: false,
+             name: 'Francais', auto: true, default: false,
              uri: 'frelo/prog_index.m3u8' }
     item = M3u8::MediaItem.new(hash)
     output = item.to_s
     expected = '#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-lo",LANGUAGE="fre",' \
-               'NAME="Français",AUTOSELECT=YES,DEFAULT=NO,' \
+               'NAME="Francais",AUTOSELECT=YES,DEFAULT=NO,' \
                'URI="frelo/prog_index.m3u8"'
     expect(output).to eq expected
   end
