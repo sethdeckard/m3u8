55fad5ad8476e78e27de4ca34ab5f2809cd5321c
Seth Deckard <seth@deckard.us>
2015-07-14 13:52:27 -0500

    Use UTC for parsed time values in spec

--- spec/time_item_spec.rb
+++ spec/time_item_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe M3u8::TimeItem do
   it 'should provide m3u8 format representation' do
-    options = { time: '2010-02-19T14:54:23.031+08:00' }
+    options = { time: '2010-02-19T14:54:23.031' }
     item = M3u8::TimeItem.new(options)
     output = item.to_s
-    expected = '#EXT-X-PROGRAM-DATE-TIME:2010-02-19T14:54:23.031+08:00'
+    expected = '#EXT-X-PROGRAM-DATE-TIME:2010-02-19T14:54:23.031'
     expect(output).to eq expected
 
-    options = { time: Time.parse('2010-02-19T14:54:23.031+08:00') }
+    options = { time: Time.iso8601('2010-02-19T14:54:23.031Z') }
     item = M3u8::TimeItem.new(options)
     output = item.to_s
-    expected = '#EXT-X-PROGRAM-DATE-TIME:2010-02-19T14:54:23+08:00'
+    expected = '#EXT-X-PROGRAM-DATE-TIME:2010-02-19T14:54:23Z'
     expect(output).to eq expected
   end
 
   it 'should parse m3u8 text into instance' do
-    input = '#EXT-X-PROGRAM-DATE-TIME:2010-02-19T14:54:23.031+08:00'
+    input = '#EXT-X-PROGRAM-DATE-TIME:2010-02-19T14:54:23.031Z'
     item = M3u8::TimeItem.parse(input)
-    expect(item.time).to eq Time.parse('2010-02-19T14:54:23.031+08:00')
+    expect(item.time).to eq Time.iso8601('2010-02-19T14:54:23.031Z')
   end
 end
