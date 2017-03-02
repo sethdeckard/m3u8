55b5d2ab4348d9ca09baae2d15d245139c7ee8e1
Seth Deckard <seth@deckard.me>
2017-03-02 10:00:10 -0600

    refactor: remove unnecessary code

--- lib/m3u8/discontinuity_item.rb
+++ lib/m3u8/discontinuity_item.rb
@@ -3,8 +3,6 @@ module M3u8
   # DiscontinuityItem represents a EXT-X-DISCONTINUITY tag to indicate a
   # discontinuity between the SegmentItems that proceed and follow it.
   class DiscontinuityItem
-    attr_accessor :tag
-
     def to_s
       "#EXT-X-DISCONTINUITY\n"
     end
--- spec/lib/m3u8/discontinuity_item_spec.rb
+++ spec/lib/m3u8/discontinuity_item_spec.rb
@@ -4,8 +4,6 @@ require 'spec_helper'
 describe M3u8::DiscontinuityItem do
   it 'should provide m3u8 format representation' do
     item = M3u8::DiscontinuityItem.new
-    output = item.to_s
-    expected = "#EXT-X-DISCONTINUITY\n"
-    expect(output).to eq expected
+    expect(item.to_s).to eq("#EXT-X-DISCONTINUITY\n")
   end
 end
