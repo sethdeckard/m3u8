7aff3958159d8c5e38692857d98e7a2fc526b66a
Seth Deckard <seth@deckard.me>
2026-02-28 16:39:25 -0500

    Add inspect for media playlists

--- lib/m3u8/cli/inspect_command.rb
+++ lib/m3u8/cli/inspect_command.rb
@@ -10,7 +10,49 @@ module M3u8
       end
 
       def run
+        if @playlist.master?
+          print_master
+        else
+          print_media
+        end
         0
+      end
+
+      private
+
+      def print_media
+        field 'Type', 'Media'
+        field 'Version', @playlist.version
+        field 'Sequence', @playlist.sequence
+        field 'Target', @playlist.target
+        field 'Duration', duration_value
+        field 'Playlist', @playlist.type
+        field 'Cache', cache_value
+        @stdout.puts
+        field 'Segments', @playlist.segments.size
+        field 'Keys', @playlist.keys.size
+        field 'Maps', @playlist.maps.size
+      end
+
+      def print_master
+        0
+      end
+
+      def duration_value
+        format('%<s>gs', s: @playlist.duration)
+      end
+
+      def cache_value
+        return unless @playlist.cache == false
+
+        'No'
+      end
+
+      def field(label, value)
+        return if value.nil?
+
+        @stdout.puts format('%-12<label>s%<value>s',
+                            label: "#{label}:", value: value)
       end
     end
   end
