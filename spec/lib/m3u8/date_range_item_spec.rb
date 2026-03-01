233bfaa41230f361b033d84ab487ff2098cc0e37
Seth Deckard <seth@deckard.me>
2026-03-01 18:28:32 -0500

    Add HLS Interstitials attributes to DateRangeItem
    Promote 9 well-defined X-ASSET-URI, X-ASSET-LIST,
    X-RESUME-OFFSET, X-PLAYOUT-LIMIT, X-RESTRICT, X-SNAP,
    X-TIMELINE-OCCUPIES, X-TIMELINE-STYLE, and X-CONTENT-MAY-VARY
    attributes from generic client_attributes to typed accessors
    with parse and format support.

--- spec/lib/m3u8/date_range_item_spec.rb
+++ spec/lib/m3u8/date_range_item_spec.rb
@@ -13,6 +13,15 @@ describe M3u8::DateRangeItem do
                   scte35_in: '0xFC002F0000000000FF1',
                   scte35_cmd: '0xFC002F0000000000FF2',
                   cue: 'PRE', end_on_next: true,
+                  asset_uri: 'http://example.com/ad.m3u8',
+                  asset_list: 'http://example.com/ads.json',
+                  resume_offset: 10.5,
+                  playout_limit: 30.0,
+                  restrict: 'SKIP,JUMP',
+                  snap: 'OUT',
+                  timeline_occupies: 'RANGE',
+                  timeline_style: 'HIGHLIGHT',
+                  content_may_vary: 'YES',
                   client_attributes: { 'X-CUSTOM' => 45.3 } }
       item = described_class.new(options)
 
@@ -27,6 +36,15 @@ describe M3u8::DateRangeItem do
       expect(item.scte35_cmd).to eq('0xFC002F0000000000FF2')
       expect(item.cue).to eq('PRE')
       expect(item.end_on_next).to be true
+      expect(item.asset_uri).to eq('http://example.com/ad.m3u8')
+      expect(item.asset_list).to eq('http://example.com/ads.json')
+      expect(item.resume_offset).to eq(10.5)
+      expect(item.playout_limit).to eq(30.0)
+      expect(item.restrict).to eq('SKIP,JUMP')
+      expect(item.snap).to eq('OUT')
+      expect(item.timeline_occupies).to eq('RANGE')
+      expect(item.timeline_style).to eq('HIGHLIGHT')
+      expect(item.content_may_vary).to eq('YES')
       expect(item.client_attributes.empty?).to be false
       expect(item.client_attributes['X-CUSTOM']).to eq(45.3)
     end
@@ -35,12 +53,21 @@ describe M3u8::DateRangeItem do
   describe '#parse' do
     it 'should parse m3u8 tag into instance' do
       item = described_class.new
-      line = '#EXT-X-DATERANGE:ID="splice-6FFFFFF0",CLASS="test_class"' \
+      line = '#EXT-X-DATERANGE:ID="splice-6FFFFFF0",CLASS="test_class",' \
              'START-DATE="2014-03-05T11:15:00Z",' \
              'END-DATE="2014-03-05T11:16:00Z",DURATION=60.1,' \
              'PLANNED-DURATION=59.993,SCTE35-OUT=0xFC002F0000000000FF0,' \
              'SCTE35-IN=0xFC002F0000000000FF1,' \
              'SCTE35-CMD=0xFC002F0000000000FF2,' \
+             'X-ASSET-URI="http://example.com/ad.m3u8",' \
+             'X-ASSET-LIST="http://example.com/ads.json",' \
+             'X-RESUME-OFFSET=10.5,' \
+             'X-PLAYOUT-LIMIT=30.0,' \
+             'X-RESTRICT="SKIP,JUMP",' \
+             'X-SNAP="OUT",' \
+             'X-TIMELINE-OCCUPIES="RANGE",' \
+             'X-TIMELINE-STYLE="HIGHLIGHT",' \
+             'X-CONTENT-MAY-VARY="YES",' \
              'CUE="PRE",' \
              'END-ON-NEXT=YES'
       item.parse(line)
@@ -54,6 +81,15 @@ describe M3u8::DateRangeItem do
       expect(item.scte35_out).to eq('0xFC002F0000000000FF0')
       expect(item.scte35_in).to eq('0xFC002F0000000000FF1')
       expect(item.scte35_cmd).to eq('0xFC002F0000000000FF2')
+      expect(item.asset_uri).to eq('http://example.com/ad.m3u8')
+      expect(item.asset_list).to eq('http://example.com/ads.json')
+      expect(item.resume_offset).to eq(10.5)
+      expect(item.playout_limit).to eq(30.0)
+      expect(item.restrict).to eq('SKIP,JUMP')
+      expect(item.snap).to eq('OUT')
+      expect(item.timeline_occupies).to eq('RANGE')
+      expect(item.timeline_style).to eq('HIGHLIGHT')
+      expect(item.content_may_vary).to eq('YES')
       expect(item.cue).to eq('PRE')
       expect(item.end_on_next).to be true
       expect(item.client_attributes.empty?).to be true
@@ -98,17 +134,38 @@ describe M3u8::DateRangeItem do
                   scte35_out: '0xFC002F0000000000FF0',
                   scte35_in: '0xFC002F0000000000FF1',
                   scte35_cmd: '0xFC002F0000000000FF2',
+                  asset_uri: 'http://example.com/ad.m3u8',
+                  asset_list: 'http://example.com/ads.json',
+                  resume_offset: 10.5,
+                  playout_limit: 30.0,
+                  restrict: 'SKIP,JUMP',
+                  snap: 'OUT',
+                  timeline_occupies: 'RANGE',
+                  timeline_style: 'HIGHLIGHT',
+                  content_may_vary: 'YES',
                   cue: 'POST,ONCE', end_on_next: true,
                   client_attributes: { 'X-CUSTOM' => 45.3,
-                                       'X-CUSTOM-TEXT' => 'test_value' } }
+                                       'X-CUSTOM-TEXT' =>
+                                         'test_value' } }
       item = described_class.new(options)
 
-      expected = '#EXT-X-DATERANGE:ID="test_id",CLASS="test_class",' \
+      expected = '#EXT-X-DATERANGE:ID="test_id",' \
+                 'CLASS="test_class",' \
                  'START-DATE="2014-03-05T11:15:00Z",' \
-                 'END-DATE="2014-03-05T11:16:00Z",DURATION=60.1,' \
+                 'END-DATE="2014-03-05T11:16:00Z",' \
+                 'DURATION=60.1,' \
                  'PLANNED-DURATION=59.993,' \
                  'X-CUSTOM=45.3,' \
                  'X-CUSTOM-TEXT="test_value",' \
+                 'X-ASSET-URI="http://example.com/ad.m3u8",' \
+                 'X-ASSET-LIST="http://example.com/ads.json",' \
+                 'X-RESUME-OFFSET=10.5,' \
+                 'X-PLAYOUT-LIMIT=30.0,' \
+                 'X-RESTRICT="SKIP,JUMP",' \
+                 'X-SNAP="OUT",' \
+                 'X-TIMELINE-OCCUPIES="RANGE",' \
+                 'X-TIMELINE-STYLE="HIGHLIGHT",' \
+                 'X-CONTENT-MAY-VARY="YES",' \
                  'SCTE35-CMD=0xFC002F0000000000FF2,' \
                  'SCTE35-OUT=0xFC002F0000000000FF0,' \
                  'SCTE35-IN=0xFC002F0000000000FF1,' \
