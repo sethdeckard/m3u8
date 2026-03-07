2ad33674b49d6e4af3424d3a2201dea52b07f5a4
Seth Deckard <seth@deckard.me>
2026-03-07 09:59:09 -0500

    Document encryption and media items
    Add YARD annotations to Encryptable, KeyItem, SessionKeyItem,
    and MediaItem classes.

--- lib/m3u8/key_item.rb
+++ lib/m3u8/key_item.rb
@@ -6,6 +6,7 @@ module M3u8
     include Encryptable
     extend M3u8
 
+    # @param params [Hash] attribute key-value pairs
     def initialize(params = {})
       options = convert_key_names(params)
       options.merge(params).each do |key, value|
@@ -13,11 +14,16 @@ module M3u8
       end
     end
 
+    # Parse an EXT-X-KEY tag.
+    # @param text [String] raw tag line
+    # @return [KeyItem]
     def self.parse(text)
       attributes = parse_attributes(text)
       KeyItem.new(attributes)
     end
 
+    # Render as an m3u8 EXT-X-KEY tag.
+    # @return [String]
     def to_s
       "#EXT-X-KEY:#{attributes_to_s}"
     end
