afa53e21a9afb5cfe9b1b3635ed70029bfeb0e70
Seth Deckard <seth@deckard.me>
2026-03-07 10:01:00 -0500

    Document map, daterange, and segment tag items
    Add YARD annotations to MapItem, DateRangeItem, ByteRange, TimeItem,
    DiscontinuityItem, GapItem, and BitrateItem.

--- lib/m3u8/discontinuity_item.rb
+++ lib/m3u8/discontinuity_item.rb
@@ -4,6 +4,8 @@ module M3u8
   # DiscontinuityItem represents a EXT-X-DISCONTINUITY tag to indicate a
   # discontinuity between the SegmentItems that proceed and follow it.
   class DiscontinuityItem
+    # Render as an m3u8 EXT-X-DISCONTINUITY tag.
+    # @return [String]
     def to_s
       "#EXT-X-DISCONTINUITY\n"
     end
