2e37f097ff1ccc8507511cb894e2032e3931b97d
Seth Deckard <seth@deckard.me>
2026-03-07 00:03:19 -0500

    Apply AttributeFormatter to DateRangeItem
    Switch from private quoted_format/unquoted_format definitions
    to include AttributeFormatter. Inline single-use format methods
    into formatted_attributes and interstitial_formats.

--- lib/m3u8/date_range_item.rb
+++ lib/m3u8/date_range_item.rb
@@ -4,6 +4,7 @@ module M3u8
   # DateRangeItem represents a #EXT-X-DATERANGE tag
   class DateRangeItem
     include M3u8
+    include AttributeFormatter
 
     attr_accessor :id, :class_name, :start_date, :end_date, :duration,
                   :planned_duration, :scte35_cmd, :scte35_out, :scte35_in,
@@ -62,34 +63,18 @@ module M3u8
 
     def formatted_attributes
       [%(ID="#{id}"),
-       class_name_format,
+       quoted_format('CLASS', class_name),
        %(START-DATE="#{start_date}"),
-       end_date_format,
-       duration_format,
-       planned_duration_format,
+       quoted_format('END-DATE', end_date),
+       unquoted_format('DURATION', duration),
+       unquoted_format('PLANNED-DURATION', planned_duration),
        client_attributes_format,
        interstitial_formats,
-       scte35_cmd_format,
-       scte35_out_format,
-       scte35_in_format,
-       cue_format,
+       unquoted_format('SCTE35-CMD', scte35_cmd),
+       unquoted_format('SCTE35-OUT', scte35_out),
+       unquoted_format('SCTE35-IN', scte35_in),
+       quoted_format('CUE', cue),
        end_on_next_format].flatten.compact.join(',')
-    end
-
-    def class_name_format
-      quoted_format('CLASS', class_name)
-    end
-
-    def end_date_format
-      quoted_format('END-DATE', end_date)
-    end
-
-    def duration_format
-      unquoted_format('DURATION', duration)
-    end
-
-    def planned_duration_format
-      unquoted_format('PLANNED-DURATION', planned_duration)
     end
 
     def client_attributes_format
@@ -97,8 +82,8 @@ module M3u8
 
       client_attributes.map do |attribute|
         value = attribute.last
-        value_format = decimal?(value) ? value : %("#{value}")
-        "#{attribute.first}=#{value_format}"
+        fmt = decimal?(value) ? value : %("#{value}")
+        "#{attribute.first}=#{fmt}"
       end
     end
 
@@ -126,77 +111,21 @@ module M3u8
     end
 
     def interstitial_formats
-      [asset_uri_format, asset_list_format,
-       resume_offset_format, playout_limit_format,
-       restrict_format, snap_format,
-       timeline_occupies_format, timeline_style_format,
-       content_may_vary_format]
-    end
-
-    def asset_uri_format
-      quoted_format('X-ASSET-URI', asset_uri)
-    end
-
-    def asset_list_format
-      quoted_format('X-ASSET-LIST', asset_list)
-    end
-
-    def resume_offset_format
-      unquoted_format('X-RESUME-OFFSET', resume_offset)
-    end
-
-    def playout_limit_format
-      unquoted_format('X-PLAYOUT-LIMIT', playout_limit)
-    end
-
-    def restrict_format
-      quoted_format('X-RESTRICT', restrict)
-    end
-
-    def snap_format
-      quoted_format('X-SNAP', snap)
-    end
-
-    def timeline_occupies_format
-      quoted_format('X-TIMELINE-OCCUPIES', timeline_occupies)
-    end
-
-    def timeline_style_format
-      quoted_format('X-TIMELINE-STYLE', timeline_style)
-    end
-
-    def content_may_vary_format
-      quoted_format('X-CONTENT-MAY-VARY', content_may_vary)
-    end
-
-    def scte35_cmd_format
-      unquoted_format('SCTE35-CMD', scte35_cmd)
-    end
-
-    def scte35_out_format
-      unquoted_format('SCTE35-OUT', scte35_out)
-    end
-
-    def scte35_in_format
-      unquoted_format('SCTE35-IN', scte35_in)
-    end
-
-    def cue_format
-      quoted_format('CUE', cue)
+      [quoted_format('X-ASSET-URI', asset_uri),
+       quoted_format('X-ASSET-LIST', asset_list),
+       unquoted_format('X-RESUME-OFFSET', resume_offset),
+       unquoted_format('X-PLAYOUT-LIMIT', playout_limit),
+       quoted_format('X-RESTRICT', restrict),
+       quoted_format('X-SNAP', snap),
+       quoted_format('X-TIMELINE-OCCUPIES', timeline_occupies),
+       quoted_format('X-TIMELINE-STYLE', timeline_style),
+       quoted_format('X-CONTENT-MAY-VARY', content_may_vary)]
     end
 
     def end_on_next_format
       return unless end_on_next
 
       'END-ON-NEXT=YES'
-    end
-
-    def quoted_format(key, value)
-      %(#{key}="#{value}") unless value.nil?
-    end
-
-    def unquoted_format(key, value)
-      "#{key}=#{value}" unless value.nil?
     end
 
     def parse_client_attributes(attributes)
